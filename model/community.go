9dd3cc99116bd09439fa98355f098bd4917defd3
Yinfeng Lu <astroluyinfeng@gmail.com>
2024-08-08 14:47:51 -0500

    finished community details api

--- model/community.go
+++ model/community.go
@@ -1,6 +1,15 @@
 package model
 
+import "time"
+
 type Community struct {
     ID   int64   `json:"id" db:"community_id"`
     Name string  `json:"name" db:"name"`
+}
+
+type CommunityDetail struct {
+    ID          int64     `json:"id" db:"community_id"`
+    Name        string    `json:"name" db:"name"`
+    Description string    `json:"description,omitempty" db:"description"`
+    CreatedTime time.Time `json:"created_time" db:"created_time"`
 }
