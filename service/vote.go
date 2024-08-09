956d64471551a33a04cc77de3d5e54a76b230622
Yinfeng Lu <astroluyinfeng@gmail.com>
2024-08-09 17:45:51 -0500

    implemented basic voting

--- service/vote.go
+++ service/vote.go
@@ -1,7 +1,10 @@
 package service
 
-import "github.com/SoonDubu923/go-forum/model"
+import (
+	"github.com/SoonDubu923/go-forum/dao/redis"
+	"github.com/SoonDubu923/go-forum/model"
+)
 
 func VoteForPost(userID int64, p *model.ParamVoteData) error {
-
+    return redis.VoteForPost(userID, p)
 }
