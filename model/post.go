33e9f672d809dbad87f9db13f84de2f96299360e
Yinfeng Lu <astroluyinfeng@gmail.com>
2024-08-09 14:58:59 -0500

    fixed bug: big ints in json

--- model/post.go
+++ model/post.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Post struct {
-    ID          int64     `json:"id" db:"post_id"`
-    AuthorID    int64     `json:"author_id" db:"author_id"`
-    CommunityID int64     `json:"community_id" db:"community_id" binding:"required"`
+    ID          int64     `json:"id,string" db:"post_id"`
+    AuthorID    int64     `json:"author_id,string" db:"author_id"`
+    CommunityID int64     `json:"community_id,string" db:"community_id" binding:"required"`
     Status      int32     `json:"status" db:"status"`
     Title       string    `json:"title" db:"title" binding:"required"`
     Content     string    `json:"content" db:"content" binding:"required"`
